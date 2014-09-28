3a103bc1c9eaf5cbf6f8d7d30ac67b0f6963ee6a
Hadi <hadichalabi@gmail.com>
2014-09-28 18:58:48 +0100

    Added 5 New Dishes

--- lib/dish.rb
+++ lib/dish.rb
@@ -6,4 +6,24 @@ def initialize(name, price)
 	@name = name
 	@price = price
 	end
+
+def self.tonkotsu
+	new("tonkotsu", 11)
+	end
+
+def self.shrimp_gyoza
+	new("shrimp gyoza", 6)
+	end
+
+def self.wagyu_beef
+	new("wagyu_beef", 35)
+	end
+
+def self.soft_shell_crab
+	new("soft shell crab", 8)
+	end
+
+def self.ebi_katsu
+	new("ebi katsu", 15)
+	end
 end
--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,7 +2,7 @@ require 'dish'
 
 describe Dish do
 
-	let(:dish){Dish.new}
+	let(:dish){Dish.new("tonkotsu", 11)}
 
 	it "should know its name" do
 	ramen = Dish.new("tonkotsu", 11)
@@ -12,5 +12,30 @@ describe Dish do
 	it "should know its price" do
 	ramen = Dish.new("tonkotsu", 11)
 	expect(ramen.price).to eq 11
+	end
+
+	it "should have a tonkotsu ramen initialization method" do
+	ramen = Dish.tonkotsu
+	expect(ramen.name).to eq("tonkotsu")
+	end
+
+	it "should have a shrimp gyoza initialization method" do
+	dumplings = Dish.shrimp_gyoza
+	expect(Dish.shrimp_gyoza.price).to eq(6)
+	end
+
+	it "should have a wagyu beef initialization method" do
+	beef = Dish.wagyu_beef
+	expect(Dish.wagyu_beef.name).to eq "wagyu_beef"
+	end
+
+	it "should have a soft shell crab initialization method" do
+	crab = Dish.soft_shell_crab
+	expect(Dish.soft_shell_crab.name).to eq "soft shell crab"
+	end
+
+	it "should have an ebi gyoza initialization method" do
+	curry = Dish.ebi_katsu
+	expect(Dish.ebi_katsu.name).to eq("ebi katsu")
 	end
 end
